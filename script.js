682b876fbcb8599138040dbd229109e92fcd05dd
Subas Kathayat <89067350+Subaskathayat@users.noreply.github.com>
2025-07-03 20:45:49 +0530

    Updated contact form for formspee

--- script.js
+++ script.js
@@ -10,3 +10,38 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('show');
   }
 });
+
+//for form
+document.getElementById("contactForm").addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const form = e.target;
+  const formData = new FormData(form);
+  const status = document.getElementById("formStatus");
+
+  try {
+    const res = await fetch(form.action, {
+      method: "POST",
+      headers: { Accept: "application/json" },
+      body: formData
+    });
+
+    if (res.ok) {
+      status.textContent = "Message sent successfully!";
+      status.style.color = "green";
+      form.reset();
+    } else {
+      const data = await res.json();
+      if (data.errors) {
+        status.textContent = data.errors.map(err => err.message).join(", ");
+      } else {
+        status.textContent = "⚠ Something went wrong.";
+      }
+      status.style.color = "red";
+    }
+  } catch (err) {
+    status.textContent = "⚠ Network error.";
+    status.style.color = "red";
+  }
+});
+
